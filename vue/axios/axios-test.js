320f5e8b44ef3a449384a19677f115842ac18d74
xiexiaozhang <1453734003@qq.com>
2020-04-23 18:08:37 +0800

    创建axios模块并发送请求

--- vue/axios/axios-test.js
+++ vue/axios/axios-test.js
@@ -1,10 +1,12 @@
 let axios = require('axios')
 let data = {
-    "username": 'james',
-    "password": "abc.123456"
-}
-
-axios.post('https://thdvuyup.lc-cn-n1-shared.com/1.1/login', data, { headers: { 'X-LC-Id': 'THdvUyuPJIEwd6DJqCY6jXcu-gzGzoHsz', 'X-LC-Key': 'Hq7b8Avv2wKzAfxRt1POfDYQ', 'Content-Type': 'application/json' } })
+        "username": 'james',
+        "password": "abc.123456"
+    }
+    //如果是第三方api需要添加下列参数
+    // { headers: { 'X-LC-Id': '应用id', 'X-LC-Key': '应用key', 'Content-Type': 'application/json' } }
+    //发送post请求
+axios.post('https://thdvuyup.lc-cn-n1-shared.com/1.1/login', data)
     .then(function(response) {
         console.log(response.data)
     }).catch(function(error) {
